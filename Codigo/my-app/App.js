f95607d12128b19db6f56336551b375cb2556a67
lauhounsss <lautarohounsfield924@gmail.com>
2025-07-04 21:20:25 -0300

    Update App.js

--- Codigo/my-app/App.js
+++ Codigo/my-app/App.js
@@ -5,16 +5,20 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './src/Login/login';
 import Registro from './src/Register/Registro';
 import Contraseñaperdida from './src/Contraseñaperdida/contraseña';
+import Perfil from './src/Perfil/perfil';
+import Feed from './src/Feed/Feed';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Registro" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName="Feed" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Registro" component={Registro} />
         <Stack.Screen name="Contraseñaperdida" component={Contraseñaperdida} />
+        <Stack.Screen name="Perfil" component={Perfil} />
+        <Stack.Screen name="Feed" component={Feed} />
       </Stack.Navigator>
     </NavigationContainer>
   );
