361570101abcc99cd620132a892a5e1a686acc74
maximogpl <maximogonzalezpolo@gmail.com>
2025-08-12 20:58:36 -0300

    Add files via upload

---
+++ Codigo/my-app/PRUEBA.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, Alert } from 'react-native';
+
+// Importa el módulo de autenticación de Firebase
+import auth from '@react-native-firebase/auth';
+
+const PRUEBA = async () => {
+
+}
+
+return{
+
+
+};
+
+export default PRUEBA; 
