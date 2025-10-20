47112d5a3d4a667e57eed36c70ce8434226383f8
manubarrabino <manuelbarrabino1@gmail.com>
2025-10-20 15:10:16 -0300

    Update App.js

--- Codigo/my-app/App.js
+++ Codigo/my-app/App.js
@@ -10,12 +10,19 @@ import Feed from './src/Feed/Feed';
 import UserConfig from './src/Configuracion user/UserConfig';
 import ConfigAdmin from './src/Config Admin/ConfigAdmin';
 import Accesibilidad from './src/Accesibilidad/Accesibilidad';
+
+// Importar las nuevas pantallas - CORREGIDO: sin espacios extras
+import LikedPosts from './src/LikedPosts/LikedPosts';
+import SharedPosts from './src/SharedPosts/SharedPosts';
+import PrivacySecurity from './src/PrivacySecurity/PrivacySecurity';
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Feed" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+        {/* Pantallas existentes */}
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Registro" component={Registro} />
         <Stack.Screen name="Contraseñaperdida" component={Contraseñaperdida} />
@@ -24,6 +31,11 @@ export default function App() {
         <Stack.Screen name="UserConfig" component={UserConfig} />
         <Stack.Screen name="ConfigAdmin" component={ConfigAdmin} />
         <Stack.Screen name="Accesbilidad" component={Accesibilidad} />
+        
+        {/* Nuevas pantallas de configuración */}
+        <Stack.Screen name="LikedPosts" component={LikedPosts} />
+        <Stack.Screen name="SharedPosts" component={SharedPosts} />
+        <Stack.Screen name="PrivacySecurity" component={PrivacySecurity} />
       </Stack.Navigator>
     </NavigationContainer>
   );
